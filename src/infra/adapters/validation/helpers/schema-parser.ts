741eb070a2c9e11a732497b2b464588086f4cded
manoel-lopes <manoel.lopes.full@gmail.com>
2024-06-29 16:50:41 -0300

    refact: zod schema parser helper class

--- src/infra/adapters/validation/helpers/schema-parser.ts
+++ src/infra/adapters/validation/helpers/schema-parser.ts
@@ -5,12 +5,14 @@ export abstract class SchemaParser {
   static parse<T>(schema: z.Schema, data: unknown): T {
     const parsedSchema = schema.safeParse(data)
     const { error } = parsedSchema
-    console.log(error?.format())
     if (error) {
       const { path, message } = error.errors[0]
-      const errorMessage = message.toLowerCase()
+      const errorMessage = message
+        .toLowerCase()
+        .replace(/\b(string|number|boolean)\b/g, '')
+        .trim()
       const field = path[1]
-      const formattedErrorMessage = `Field '${field}' ${errorMessage === 'required' ? 'is ' + errorMessage : errorMessage}`
+      const formattedErrorMessage = `Field '${field}' ${errorMessage === 'required' ? 'is' + errorMessage : errorMessage}`
       throw new SchemaParseFailedError(formattedErrorMessage)
     }
     return parsedSchema.data
