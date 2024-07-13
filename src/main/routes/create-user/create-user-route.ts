2a4a762f78a5885331d9d50bde4cf52220017b58
manoel-lopes <manoel.lopes.full@gmail.com>
2024-07-13 15:11:22 -0300

    feat: create user controller factory

--- src/main/routes/create-user/create-user-route.ts
+++ src/main/routes/create-user/create-user-route.ts
@@ -1,34 +1,10 @@
 import type { HttpServer } from '@/infra/adapters/http/http-server/ports'
-import { CreateUserController } from '@/presentation/controllers/create-user/create-user-controller'
-import { CreateUserUseCase } from '@/application/usecases/create-user/create-user-use-case'
-import { CreateUserZodSchemaValidator } from '@/infra/adapters/validation/schemas/create-user-zod-schema-validator'
-import { PrismaUserRepository } from '@/infra/repositories/prisma/prisma-user-repository'
-import { BcryptPasswordEncryptor } from '@/infra/adapters/password-encryptor/bcrypt/bcrypt-password-encryptor'
-
-function makeCreateUserController(): {
-  createUserController: CreateUserController
-} {
-  const prismaUserRepository = new PrismaUserRepository()
-  const passwordEncryptor = new BcryptPasswordEncryptor()
-  const createUserUseCase = new CreateUserUseCase(
-    prismaUserRepository,
-    passwordEncryptor,
-  )
-  const createUserSchemaValidator = new CreateUserZodSchemaValidator()
-  const createUserController = new CreateUserController(
-    createUserUseCase,
-    createUserSchemaValidator,
-  )
-  return {
-    createUserController,
-  }
-}
+import { makeCreateUserController } from '@/main/factories/create-user-controller-factory'
 
 export function setCreateUserRoute(app: HttpServer) {
   app.route('POST', '/users', async (req, res) => {
-    const { createUserController } = makeCreateUserController()
+    const createUserController = makeCreateUserController()
     const { statusCode, body } = await createUserController.handle(req)
-    if (body) return res.status(statusCode).json(body)
-    return res.status(statusCode).json()
+    return res.status(statusCode).json(body)
   })
 }
