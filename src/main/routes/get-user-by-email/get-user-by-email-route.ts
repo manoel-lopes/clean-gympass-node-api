f9727e0f0a2de4c46da633ec2fce2db4f067ec68
manoel-lopes <manoel.lopes.full@gmail.com>
2024-07-13 16:19:09 -0300

    fix(infra/prisma): 'created_at' property from finded user on 'findByEmail' method from prisma user repository

--- src/main/routes/get-user-by-email/get-user-by-email-route.ts
+++ src/main/routes/get-user-by-email/get-user-by-email-route.ts
@@ -2,7 +2,7 @@ import type { HttpServer } from '@/infra/adapters/http/http-server/ports'
 import { makeGetUserByEmailController } from '@/main/factories/get-user-by-email-controller-factory'
 
 export function setGetUserByEmailRoute(app: HttpServer) {
-  app.route('GET', '/users', async (req, res) => {
+  app.route('GET', '/users/:email', async (req, res) => {
     const getUserByEmailController = makeGetUserByEmailController()
     const { statusCode, body } = await getUserByEmailController.handle(req)
     return res.status(statusCode).json(body)
