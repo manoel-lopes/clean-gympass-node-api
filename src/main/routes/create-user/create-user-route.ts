20044f29277b5903bd88ac82ba7110f429ed7bfa
manoel-lopes <manoel.lopes.full@gmail.com>
2024-07-15 18:13:09 -0300

    refact(routes): create user route

--- src/main/routes/create-user/create-user-route.ts
+++ src/main/routes/create-user/create-user-route.ts
@@ -2,7 +2,7 @@ import type { HttpServer } from '@/infra/adapters/http/http-server/ports'
 import { makeCreateUserController } from '@/main/factories/create-user-controller-factory'
 
 export function setCreateUserRoute(app: HttpServer) {
-  app.route('POST', '/users', async (req, res) => {
+  app.route('POST', '/user', async (req, res) => {
     const createUserController = makeCreateUserController()
     const { statusCode, body } = await createUserController.handle(req)
     return res.status(statusCode).json(body)
