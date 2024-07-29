f2f5abce4341276990d8fe6a400772a4ab9856fb
manoel-lopes <manoel.lopes.full@gmail.com>
2024-07-28 21:04:47 -0300

    style: renaming 'create-check-in' use case directory from plural to singular

--- src/application/usecases/check-ins/index.ts
+++ src/application/usecases/check-ins/index.ts
@@ -1,2 +1,2 @@
-export * from './create-check-ins/create-check-in-use-case'
+export * from './create-check-in/create-check-in-use-case'
 export * from './get-user-check-ins-history/get-user-check-ins-history-use-case'
