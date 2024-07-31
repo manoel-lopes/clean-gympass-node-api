de6a57b3ac1bcc9640d9101319ee6139f58665a4
manoel-lopes <manoel.lopes.full@gmail.com>
2024-07-31 13:21:07 -0300

    refactor(tests): get check ins history use case tests suite

--- src/application/usecases/check-ins/get-check-ins-history/get-check-ins-history.test.ts
+++ src/application/usecases/check-ins/get-check-ins-history/get-check-ins-history.test.ts
@@ -3,21 +3,16 @@ import { InMemoryCheckInsRepository } from '@/infra/repositories/in-memory/in-me
 import { InexistentRegisteredUser } from '@/application/errors'
 import { GetCheckInsHistoryUseCase } from './get-check-ins-history'
 
-type Sut = {
-  sut: GetCheckInsHistoryUseCase
-}
-
-function makeSut(): Sut {
-  const usersRepository = new InMemoryUsersRepository()
-  const checkInsRepository = new InMemoryCheckInsRepository()
-  const sut = new GetCheckInsHistoryUseCase(usersRepository, checkInsRepository)
-  return { sut }
-}
-
 describe('GetCheckInsHistoryUseCase', () => {
+  let sut: GetCheckInsHistoryUseCase
+  beforeEach(() => {
+    const usersRepository = new InMemoryUsersRepository()
+    const checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new GetCheckInsHistoryUseCase(usersRepository, checkInsRepository)
+  })
+
   it('should throw an error if there is not a registered user with the given user id', () => {
     const userId = 'any_non_existing_user_id'
-    const { sut } = makeSut()
 
     expect(sut.execute({ userId })).rejects.toThrowError(
       new InexistentRegisteredUser('id'),
