0b856e9e90379e081192d629712c5d8e022e8403
manoel-lopes <manoel.lopes.full@gmail.com>
2024-08-02 15:22:08 -0300

    refactor(tests): authenticate user use case tests suite

--- src/application/usecases/users/authenticate-user/authenticate-user-use-case.test.ts
+++ src/application/usecases/users/authenticate-user/authenticate-user-use-case.test.ts
@@ -1,6 +1,5 @@
 import { vi, describe, it, expect } from 'vitest'
 
-import { CreateUserUseCase } from '@/application/usecases/users'
 import { InexistentRegisteredUser } from '@/application/errors'
 import type { PasswordEncryptor } from '@/infra/adapters/password-encryptor/ports'
 import { InMemoryUsersRepository } from '@/infra/repositories/in-memory/in-memory-users-repository'
@@ -8,47 +7,27 @@ import { PasswordEncryptorStub } from '@/infra/adapters/password-encryptor/stub/
 import { AuthenticateUserUseCase } from './authenticate-user-use-case'
 import { InvalidPasswordError } from './errors'
 
-type Sut = {
-  sut: AuthenticateUserUseCase
-  createUserUseCase: CreateUserUseCase
-  passwordEncryptorStub: PasswordEncryptor
-}
-
-function makeSut(): Sut {
-  const passwordEncryptorStub = new PasswordEncryptorStub()
-  const UsersRepository = new InMemoryUsersRepository()
-  const createUserUseCase = new CreateUserUseCase(
-    UsersRepository,
-    passwordEncryptorStub,
-  )
-  const sut = new AuthenticateUserUseCase(
-    UsersRepository,
-    passwordEncryptorStub,
-  )
-  return {
-    sut,
-    createUserUseCase,
-    passwordEncryptorStub,
-  }
-}
-
 describe('AuthenticateUserUseCase', () => {
+  let passwordEncryptorStub: PasswordEncryptor
+  let sut: AuthenticateUserUseCase
   const request = {
     email: 'any_email',
     password: 'any_password',
   }
+  beforeEach(async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    passwordEncryptorStub = new PasswordEncryptorStub()
+    sut = new AuthenticateUserUseCase(usersRepository, passwordEncryptorStub)
+    await usersRepository.save({ name: 'any_name', ...request })
+  })
 
   it('should throw an error if there is not a registered user with the given email', () => {
-    const { sut } = makeSut()
-
-    expect(sut.execute(request)).rejects.toThrowError(
-      new InexistentRegisteredUser('email'),
-    )
+    expect(
+      sut.execute({ ...request, email: 'any_non_existent_user_email' }),
+    ).rejects.toThrowError(new InexistentRegisteredUser('email'))
   })
 
   it('should throw an error if the given password does not match the stored password', async () => {
-    const { sut, createUserUseCase, passwordEncryptorStub } = makeSut()
-    await createUserUseCase.execute({ name: 'any_name', ...request })
     vi.spyOn(passwordEncryptorStub, 'verifyPassword').mockResolvedValueOnce(
       false,
     )
@@ -56,5 +35,14 @@ describe('AuthenticateUserUseCase', () => {
     await expect(sut.execute(request)).rejects.toThrowError(
       new InvalidPasswordError(),
     )
+  })
+
+  it('should correctly authenticate an user', async () => {
+    const user = await sut.execute(request)
+
+    expect(user.id).toEqual(expect.any(String))
+    expect(user.name).toBe('any_name')
+    expect(user.email).toBe(request.email)
+    expect(new Date(user.createdAt).getTime()).not.toBeNaN()
   })
 })
