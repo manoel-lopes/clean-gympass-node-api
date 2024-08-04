6ab0ac21c979d1c423d4105577f40c99e6177a2d
manoel-lopes <manoel.lopes.full@gmail.com>
2024-08-04 10:08:05 -0300

    refactor(tests): get user by id use case tests suite

--- src/application/usecases/users/get-user-by-id/get-user-by-id-use-case.test.ts
+++ src/application/usecases/users/get-user-by-id/get-user-by-id-use-case.test.ts
@@ -1,59 +1,30 @@
 import { describe, it, expect } from 'vitest'
 
 import { InMemoryUsersRepository } from '@/infra/repositories/in-memory/in-memory-users-repository'
-import { PasswordEncryptorStub } from '@/infra/adapters/password-encryptor/stub/password-encryptor-stub'
 import { InexistentRegisteredUser } from '@/application/errors'
-import {
-  CreateUserUseCase,
-  AuthenticateUserUseCase,
-} from '@/application/usecases/users'
 import { GetUserByIdUseCase } from './get-user-by-id-use-case'
 
-type Sut = {
-  sut: GetUserByIdUseCase
-  createUserUseCase: CreateUserUseCase
-  authenticateUserUseCase: AuthenticateUserUseCase
-}
-
-function makeSut(): Sut {
-  const UsersRepository = new InMemoryUsersRepository()
-  const passwordEncryptor = new PasswordEncryptorStub()
-  const sut = new GetUserByIdUseCase(UsersRepository)
-  const createUserUseCase = new CreateUserUseCase(
-    UsersRepository,
-    passwordEncryptor,
-  )
-  const authenticateUserUseCase = new AuthenticateUserUseCase(
-    UsersRepository,
-    passwordEncryptor,
-  )
-  return { sut, createUserUseCase, authenticateUserUseCase }
-}
-
 describe('GetUserByIdUseCase', () => {
-  it('should throw an error if there is not a registered user with the given user id', () => {
-    const userId = 'any_non_existing_user_id'
-    const { sut } = makeSut()
-
-    expect(sut.execute({ userId })).rejects.toThrowError(
-      new InexistentRegisteredUser('id'),
-    )
-  })
-
-  it('should correctly return the user data', async () => {
-    const { sut, createUserUseCase, authenticateUserUseCase } = makeSut()
-
-    await createUserUseCase.execute({
+  let sut: GetUserByIdUseCase
+  const userId = 'any_id'
+  beforeEach(async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserByIdUseCase(usersRepository)
+    await usersRepository.save({
+      id: userId,
       name: 'any_name',
       email: 'any_email',
       password: 'any_password',
     })
+  })
 
-    const { id: userId } = await authenticateUserUseCase.execute({
-      email: 'any_email',
-      password: 'any_password',
-    })
+  it('should throw an error if there is not a registered user with the given user id', () => {
+    expect(
+      sut.execute({ userId: 'any_non_existing_user_id' }),
+    ).rejects.toThrowError(new InexistentRegisteredUser('id'))
+  })
 
+  it('should correctly return the user data', async () => {
     const user = await sut.execute({ userId })
 
     expect(user.id).toBe(userId)
