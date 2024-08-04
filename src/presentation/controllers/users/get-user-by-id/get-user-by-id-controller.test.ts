8dfafb4cb2d1b628c6340bb30af9c4626e09ebe7
manoel-lopes <manoel.lopes.full@gmail.com>
2024-08-04 15:21:10 -0300

    refactor(tests): get user by id controller tests suite

--- src/presentation/controllers/users/get-user-by-id/get-user-by-id-controller.test.ts
+++ src/presentation/controllers/users/get-user-by-id/get-user-by-id-controller.test.ts
@@ -6,12 +6,6 @@ import { SchemaValidatorStub } from '@/infra/adapters/validation/schemas/stub/sc
 import { SchemaParseFailedError } from '@/infra/adapters/validation/errors'
 import { GetUserByIdController } from './get-user-by-id-controller'
 
-type Sut = {
-  sut: GetUserByIdController
-  getUserByIdUseCase: UseCase
-  getUserByIdSchemaValidator: SchemaValidator
-}
-
 function makeGetUserByIdSub(): UseCase {
   class GetUserByIdStub implements UseCase {
     async execute(): Promise<GetUserByIdResponse> {
@@ -27,21 +21,22 @@ function makeGetUserByIdSub(): UseCase {
   return new GetUserByIdStub()
 }
 
-function makeSut(): Sut {
-  const getUserByIdUseCase = makeGetUserByIdSub()
-  const getUserByIdSchemaValidator = new SchemaValidatorStub()
-  const sut = new GetUserByIdController(
-    getUserByIdUseCase,
-    getUserByIdSchemaValidator,
-  )
-  return { sut, getUserByIdUseCase, getUserByIdSchemaValidator }
-}
-
 describe('GetUserByIdController', () => {
-  const httpRequest = { params: { userId: 'any_Id' } }
+  let getUserByIdUseCase: UseCase
+  let getUserByIdSchemaValidator: SchemaValidator
+  let sut: GetUserByIdController
+  const httpRequest = { params: { userId: 'any_id' } }
+
+  beforeEach(() => {
+    getUserByIdUseCase = makeGetUserByIdSub()
+    getUserByIdSchemaValidator = new SchemaValidatorStub()
+    sut = new GetUserByIdController(
+      getUserByIdUseCase,
+      getUserByIdSchemaValidator,
+    )
+  })
 
   it('should return 404 if there is not a user with the given user id', async () => {
-    const { sut, getUserByIdUseCase } = makeSut()
     vi.spyOn(getUserByIdUseCase, 'execute').mockRejectedValue(
       new InexistentRegisteredUser('id'),
     )
@@ -56,7 +51,6 @@ describe('GetUserByIdController', () => {
   })
 
   it('should throw a schema validation error', async () => {
-    const { sut, getUserByIdSchemaValidator } = makeSut()
     vi.spyOn(getUserByIdSchemaValidator, 'validate').mockImplementation(() => {
       throw new SchemaParseFailedError('any_error')
     })
@@ -67,7 +61,6 @@ describe('GetUserByIdController', () => {
   })
 
   it('should throw the error if is not a known error', async () => {
-    const { sut, getUserByIdUseCase } = makeSut()
     vi.spyOn(getUserByIdUseCase, 'execute').mockRejectedValue(
       new Error('any_error'),
     )
@@ -76,8 +69,6 @@ describe('GetUserByIdController', () => {
   })
 
   it('should return 200 and the user data on success', async () => {
-    const { sut } = makeSut()
-
     const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(200)
